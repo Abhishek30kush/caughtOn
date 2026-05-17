f87fdd063a2900b6da84d51f1e430d91986aaaa2
Abhishek Kushwaha <abhi.2010.kushwaha@gmail.com>
2026-05-17 20:48:27 +0530

    security: relocate hardcoded Firebase configuration secrets to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBN2Ec3O-wX2edx3KH0c7vXDTLigcC_TVA",
-  authDomain: "caughton-c3900.firebaseapp.com",
-  projectId: "caughton-c3900",
-  storageBucket: "caughton-c3900.firebasestorage.app",
-  messagingSenderId: "517220216889",
-  appId: "1:517220216889:web:1dc920b08ad8315ab79de8",
-  measurementId: "G-C0RYFVD33J"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
